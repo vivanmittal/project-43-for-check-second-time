222f10ea33781caa653f09fa271e1d482fe0ddf3
vivanmittal <75486816+vivanmittal@users.noreply.github.com>
2021-07-24 10:58:13 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,15 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+
+}
