08a84c371b59e677aafe62b6261dda9cdb20d89f
vivanmittal <75486816+vivanmittal@users.noreply.github.com>
2021-08-03 22:24:11 +0530

    Add files via upload

--- System.js
+++ System.js
@@ -7,6 +7,10 @@ class System{
         fill("black");
         text(code,300,300)
         //add code to return true if correct answer entered.
+if(
+    actualCode===enteredCode.toUpperCase()){
+return true
+    }else{return false}
 
         
     }
